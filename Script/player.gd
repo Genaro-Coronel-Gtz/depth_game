993da56563fa7810ad36fd641d2142c4aa8626b4
Genaro-Coronel-Gtz <sk_8_917@hotmail.com>
2025-04-05 11:35:07 -0600

    [Wip]: Switch cameras

--- Script/player.gd
+++ Script/player.gd
@@ -5,40 +5,33 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var mouse_sensitivity = 0.00050
 
 @export var jump_velocity = 6.0
-@onready var minimap_rect : TextureRect = $TextureRect
-#@onready var minimap_rect : TextureRect = $SubViewport/TextureRect
-@onready var capture_subviewport: SubViewport = $SubViewportContainer/SubViewport
-@onready var capture_camera: Camera3D = $SubViewportContainer/SubViewport/CaptureCamera
+@onready var minimap_rect : TextureRect = $CanvasLayer/Control/TextureRect
+@onready var capture_subviewport: SubViewport = $SubViewport
+@onready var capture_camera: Camera3D = $SubViewport/CaptureCamera
 
-@onready var subviewport_container: SubViewportContainer = $SubViewportContainer
 var capture_camera_active = false
 
 var shader_material : ShaderMaterial
 var is_blurred : bool = false
 
 func _init_blur_configs():
-	var blur_shader = load("res://Shaders/blur_shader.gdshader")
+	capture_subviewport.render_target_update_mode = SubViewport.UPDATE_ALWAYS
+	var blur_shader = load("res://Shaders/old_camera.gdshader")
 	shader_material = ShaderMaterial.new()
 	shader_material.shader = blur_shader
-	shader_material.set_shader_parameter("blur_size", 5)
+	#shader_material.set_shader_parameter("blur_size", 1)
 	minimap_rect.material = shader_material
-	subviewport_container.visible = false
+	
+	minimap_rect.texture = capture_subviewport.get_texture()
 	
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	capture_subviewport.render_target_update_mode = SubViewport.UPDATE_ALWAYS
-	
 	_init_blur_configs()
-	minimap_rect.texture = capture_subviewport.get_texture()
-
-func _update_preview():
-	minimap_rect.texture = capture_subviewport.get_texture()
-
+	
 func _process(delta) -> void:
-	_copy_cameras($Camera3D, capture_camera)
-	#_update_preview()
+	_sync_cameras($Camera3D, capture_camera)
 
-func _copy_cameras(camera_from: Camera3D, camera_to: Camera3D):
+func _sync_cameras(camera_from: Camera3D, camera_to: Camera3D):
 	camera_to.global_transform.origin = camera_from.global_transform.origin
 	camera_to.global_transform.basis = camera_from.global_transform.basis
 
@@ -53,8 +46,6 @@ func _input(event):
 		$Camera3D.rotate_x(-event.relative.y * mouse_sensitivity)
 		$Camera3D.rotation.x = clampf($Camera3D.rotation.x, -deg_to_rad(70), deg_to_rad(70))
 		
-		#capture_camera.rotate_x(-event.relative.y * mouse_sensitivity)
-		#capture_camera.rotation.x = clampf(capture_camera.rotation.x, -deg_to_rad(70), deg_to_rad(70))	
 		
 	if event.is_action_pressed("change_cam"):
 		_toggle_cam()
@@ -65,25 +56,34 @@ func _input(event):
 func _toggle_cam():
 	print("toggle camera")
 	
-	_copy_cameras($Camera3D, capture_camera)
+	_sync_cameras($Camera3D, capture_camera)
 	
-	if not capture_camera_active:
+	if capture_camera_active:
 		$Camera3D.current = false
 		capture_camera.current = true
-		subviewport_container.visible = true
+		minimap_rect.visible = true
 	else:
 		$Camera3D.current = true
 		capture_camera.current = false
-		subviewport_container.visible = false
+		minimap_rect.visible = false
 
 	capture_camera_active = !capture_camera_active
 
 func _shoot_cam():
-	_copy_cameras($Camera3D, capture_camera)
+	_sync_cameras($Camera3D, capture_camera)
 	
 	await RenderingServer.frame_post_draw
 	
-	var image = capture_subviewport.get_viewport().get_texture().get_image()
+	var image = null
+	
+	image = minimap_rect.get_viewport().get_texture().get_image()
+	#image = capture_subviewport.get_viewport().get_texture().get_image()
+	
+	#if capture_camera_active:
+		#image = minimap_rect.get_viewport().get_texture().get_image()
+	#else:
+		#image = capture_subviewport.get_viewport().get_texture().get_image()
+		
 	var timestamp = Time.get_datetime_string_from_system().replace(":", "-")
 	var path = "res://screen_shots/captura_" + timestamp + ".png"
 	
